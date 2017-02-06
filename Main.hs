3ea59a32b58b7e09936d5de2a0b814407ffde9ea
root <hawk.alan@gmail.com>
2017-02-06 01:21:03 +0000

    added extra files

--- Main.hs
+++ Main.hs
@@ -2,10 +2,12 @@ import System.IO
 import Control.Monad
 
 main = main' 0
-main' counter  = do
+main' counter = do
  p <- hIsEOF stdin
  when (not p) $ do
   x <- getLine
   let newLength = length x
-  putStrLn (show counter ++ " " ++ show newLength ++ " " ++ take 50 x ++ "..")
+  let ellipsis = if newLength > 50 then ".." else ""
+  let message = show counter ++ " " ++ show newLength ++ " " ++ take 50 x ++ ellipsis
+  putStrLn message
   main' (succ counter)
