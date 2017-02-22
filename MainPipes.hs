c160eb1710df835b856d8898ce6c7a3e2761fb61
xpika <hawk.alan@gmail.com>
2017-02-22 12:06:24 +1100

    mod

--- MainPipes.hs
+++ MainPipes.hs
@@ -1,8 +1,8 @@
 import Pipes
 import qualified Pipes.Prelude as PP
 
-main = runEffect $ for (PP.zip (each [1..]) PP.stdinLn) g
-g (counter,line) = lift (putStrLn message)
+main = runEffect $ (PP.zip (each [1..]) PP.stdinLn) >-> PP.map g >-> PP.stdoutLn
+g (counter,line) = show counter ++ " " ++ show newLength ++ " " ++ take 50  line ++ ellipsis
    where newLength = length line
          ellipsis = if newLength > 50 then ".." else ""
-         message = show counter ++ " " ++ show newLength ++ " " ++ take 50  line ++ ellipsis
+ 
