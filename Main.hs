1bad23fcd6d5f4b69ac7d08aab322dcfa329eb23
MikeH <matroxmike@icloud.com>
2017-03-02 15:51:58 +1100

    ellipsis is generally understood to be 3 dots

--- Main.hs
+++ Main.hs
@@ -4,10 +4,11 @@ import Control.Monad
 main = main' 0
 main' counter = do
  p <- hIsEOF stdin
- when (not p) $ do
+ -- to swap above line to IsEOF requires extra import
+ unless p $ do
   x <- getLine
   let newLength = length x
-  let ellipsis = if newLength > 50 then ".." else ""
+  let ellipsis = if newLength > 50 then "..." else ""
   let message = show counter ++ " " ++ show newLength ++ " " ++ take 50 x ++ ellipsis
   putStrLn message
   main' (succ counter)
