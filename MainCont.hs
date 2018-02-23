4b6bc1b885aa0a37cc92844d9ab08b3e9c2e9323
Alan Hawkins <alanpika_au@yahoo.com>
2018-02-23 22:16:28 +1100

    Update MainCont.hs

--- MainCont.hs
+++ MainCont.hs
@@ -5,9 +5,10 @@ main = (`runContT` return) $ do
     callCC $ \exit -> do
       forM_ [1..] $ \counter -> do
         p <- lift $ hIsEOF stdin
+        when p $ exit () 
         x <- lift getLine
         let newLength = length x
         let ellipsis = if newLength > 50 then ".." else ""
         let message = show counter ++ " " ++ show newLength ++ " " ++ take 50 x ++ ellipsis
         lift $ putStrLn message
-        when p $ exit () 
+
